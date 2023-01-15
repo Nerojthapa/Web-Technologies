dd182ef89b6bbaed91f77b7f5e834f32e2c06a13
Niroj Thapa <nirojthapa485@gmail.com>
2023-01-15 21:20:16 +0545

    starting with js week 9 workshop

---
+++ week 9/js/script.js
@@ -0,0 +1,27 @@
+
+        // alert("Hi I am alert");
+        // console.log("hi i am 20 years old");
+        // console.log("Hi i am niroj");
+        // console.log(document.getElementById("paragraph").innerHTML);
+        // document.getElementById("paragraph").innerHTML="<h1>Basantapur</h1>"
+        // var nirojthapa="Hi I am niroj Thapa";
+        // console.log("nirojthapa");
+        // console.log(nirojthapa);
+        // document.getElementById("my_message").innerHTML="yahoo"+" "+"nirojthapa"+"!";
+        
+        
+    // var age=40;
+    // var firt_name="niroj";
+    // var last_name="thapa";
+    // var salary=10000.489;
+    // var phone_number="9869551984";
+    // console.log(age);
+    function sum(a,b){
+        // var a=10;
+        // var b=20;
+        var sum=a+b;
+        console.log(sum);
+    }
+    sum(10,5);
+    sum(50,50);
+    sum(100,20);
